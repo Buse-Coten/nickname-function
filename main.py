1e1c2c8c4f312924ec37a976d0b032b0d0cd8846
Buse Çöten <95180221+Buse-Coten@users.noreply.github.com>
2022-05-29 21:51:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input("What is your name?\n")
+nickname = input("And.. what is your name?\n")
+def meet(name,nickname):
+  print(f"Hello {name}!")
+  print(f"Maybe i should say {nickname}!")
+
+
+meet(f"{name}",f"{nickname}")
